f6815738491ccb9203929f6693af34e39eccf271
Lukáš Lalinský <lalinsky@gmail.com>
2019-04-23 14:12:49 +0200

    Test cache

--- build-image.sh
+++ build-image.sh
@@ -7,6 +7,7 @@ PATRONI_VERSION=1.5.6
 
 IMAGE=quay.io/acoustid/postgres-patroni
 
+
 docker pull $IMAGE:$CI_COMMIT_REF_SLUG || true
 docker build --cache-from=$IMAGE:$CI_COMMIT_REF_SLUG -t $IMAGE:$CI_COMMIT_REF_SLUG --build-arg=PG_VERSION=$PG_VERSION --build-arg=PATRONI_VERSION=$PATRONI_VERSION .
 docker push $IMAGE:$CI_COMMIT_REF_SLUG
