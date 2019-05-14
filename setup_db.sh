8740bd0fcc67f64d880d27b1e583dd75c1035f72
Lukáš Lalinský <lalinsky@gmail.com>
2019-05-14 12:26:36 +0200

    Add the earthdistance extension

--- setup_db.sh
+++ setup_db.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 exec psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EOS
-CREATE EXTENSION cube;
 CREATE EXTENSION pgcrypto;
 CREATE EXTENSION intarray;
 CREATE EXTENSION acoustid;
+
+-- for musicbrainz
+CREATE EXTENSION cube;
+CREATE EXTENSION earthdistance;
 EOS
