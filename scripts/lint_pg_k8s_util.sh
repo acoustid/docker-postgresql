f32ec6bfa25d02d6f880b920a604ba1ae06858b6
Lukáš Lalinský <lalinsky@gmail.com>
2022-12-10 08:25:29 +0100

    Remove old scripts

--- scripts/lint_pg_k8s_util.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-flake8 --max-line-length=120 pg_k8s_util
-mypy pg_k8s_util
--- scripts/push-image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-echo "$QUAY_PASSWORD" | docker login quay.io --username "$QUAY_USERNAME" --password-stdin
-
-set -x
-
-docker push $IMAGE:$VERSION
